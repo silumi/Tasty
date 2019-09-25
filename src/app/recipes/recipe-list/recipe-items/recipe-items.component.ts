f34061a4e53c03a8ba50ed464884aaed92cb01fa
silumi <silumikaundika@gmail.com>
2019-09-25 08:58:33 +0530

    Databinding- Property binding

--- src/app/recipes/recipe-list/recipe-items/recipe-items.component.ts
+++ src/app/recipes/recipe-list/recipe-items/recipe-items.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {Recipe} from '../../Recipe.model';
 
 @Component({
   selector: 'app-recipe-items',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-items.component.css']
 })
 export class RecipeItemsComponent implements OnInit {
-
+  @Input() recipe: Recipe;
   constructor() { }
 
   ngOnInit() {
